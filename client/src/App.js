25bc383f8a8853836d98cd2cb265c50b18786d7e
Anthony Salemo <ajssalemo@gmail.com>
2020-10-24 16:05:48 -0400

    interactive map is now displayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import DisplayMap from "./components/DisplayMap";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <DisplayMap />
     </div>
   );
 }
---
+++ client/src/components/DisplayMap.js
@@ -0,0 +1,38 @@
+import { useLayoutEffect, useRef } from "react";
+
+// This code is referenced from - https://developer.here.com/tutorials/react/#a-note-on-hooks
+export default function DisplayMap() {
+  // Create a reference to the HTML element we want to put the map on
+  const mapRef = useRef(null);
+  /**
+   * Create the map instance
+   * While `useEffect` could also be used here, `useLayoutEffect` will render
+   * the map sooner
+   */
+  useLayoutEffect(() => {
+    // `mapRef.current` will be `undefined` when this hook first runs; edge case that
+    if (!mapRef.current) return;
+    const H = window.H;
+    const platform = new H.service.Platform({
+      apikey: `${process.env.REACT_APP_HERE_DEVELOPER_MAP_API_KEY}`,
+    });
+    const defaultLayers = platform.createDefaultLayers();
+    const hMap = new H.Map(mapRef.current, defaultLayers.vector.normal.map, {
+      center: { lat: 50, lng: 5 },
+      zoom: 4,
+      pixelRatio: window.devicePixelRatio || 1,
+    });
+    // These two variables are unused on purpose 
+    // eslint-disable-next-line no-unused-vars
+    const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(hMap));
+    // eslint-disable-next-line no-unused-vars
+    const ui = H.ui.UI.createDefault(hMap, defaultLayers);
+    // This will act as a cleanup to run once this hook runs again.
+    // This includes when the component un-mounts
+    return () => {
+      hMap.dispose();
+    };
+  }, [mapRef]); // This will run this hook every time this ref is updated
+
+  return <div className="map" ref={mapRef} style={{ height: "500px" }} />;
+}
