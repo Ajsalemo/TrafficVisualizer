0c5b9354fcca2bda6f76ebae901211eec83625fc
Anthony Salemo <ajssalemo@gmail.com>
2020-11-01 21:08:56 -0500

    able to authenticate with Auth0

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import Dropdown from "../Dropdown/Dropdown";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <nav className="inset-x-0 top-0 text-white h-12 md:flex">
       <div className="hidden md:flex w-full flex justify-between items-center px-4">
         <Link to="/dashboard">Dashboard</Link>
-        <Link to="/">Sign in</Link>
+        <button onClick={() => loginWithRedirect()}>
+          Sign in
+        </button>
       </div>
       {/* Dropdown for mobile viewing */}
       <Dropdown />
