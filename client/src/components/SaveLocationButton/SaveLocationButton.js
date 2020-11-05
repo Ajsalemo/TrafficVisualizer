26910d0bbd1ec437991ca4231ac2a21c4d76562d
Anthony Salemo <ajssalemo@gmail.com>
2020-11-05 18:05:23 -0500

    added a conditional statement to display the savel location button depending on authentication status

--- client/src/components/SaveLocationButton/SaveLocationButton.js
+++ client/src/components/SaveLocationButton/SaveLocationButton.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const saveLocationToAccount = async (addressValue) => {
   const postLocation = await axios.post("/api/test_route", {
@@ -9,14 +10,15 @@ const saveLocationToAccount = async (addressValue) => {
 };
 
 const SaveLocationButton = ({ addressValue }) => {
-  return (
+  const { isAuthenticated } = useAuth0();
+  return isAuthenticated ? (
     <button
       className="focus:outline-none focus:shadow-outline rounded-full py-2 px-4 bg-blue-900 text-white mt-4 mx-auto w-1/2 sm:w-1/4"
       onClick={() => saveLocationToAccount(addressValue)}
     >
       Save location
     </button>
-  );
+  ) : null;
 };
 
 export default SaveLocationButton;
