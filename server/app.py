d6168651e9650d7fa964d30a29d7378c4730a702
Anthony Salemo <ajssalemo@gmail.com>
2020-11-14 16:51:09 -0500

    split up app.py into another file to separate logic

--- server/app.py
+++ server/app.py
@@ -1,42 +1,9 @@
-import os
-
-from dotenv import load_dotenv
-from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
-from flask_migrate import Migrate
+from flask import jsonify, request
 
+from manage import app
 from models import User, Locations, db
 
-# Load dotenv
-load_dotenv()
-
-# Local Postgres environment
-POSTGRES_USERNAME = os.getenv('POSTGRES_USERNAME')
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-POSTGRES_HOST = os.getenv('POSTGRES_HOST')
-POSTGRES_PORT = os.getenv('POSTGRES_PORT')
-POSTGRES_DATABASE = os.getenv('POSTGRES_DATABASE')
-# Production POstgres environment
-AZURE_POSTGRES_USERNAME = os.getenv('AZURE_POSTGRES_USERNAME')
-AZURE_POSTGRES_PASSWORD = os.getenv('AZURE_POSTGRES_PASSWORD')
-AZURE_POSTGRES_HOST = os.getenv('AZURE_POSTGRES_HOST')
-AZURE_POSTGRES_PORT = os.getenv('AZURE_POSTGRES_PORT')
-AZURE_POSTGRES_DATABASE = os.getenv('AZURE_POSTGRES_DATABASE')
-
-app = Flask(__name__)
-cors = CORS(app)
-
-# Local connection string
-local_conn_str = f"postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}"
-# Production connection string
-prod_conn_str = f"postgresql://{AZURE_POSTGRES_USERNAME}:{AZURE_POSTGRES_PASSWORD}@{AZURE_POSTGRES_HOST}:{AZURE_POSTGRES_PORT}/{AZURE_POSTGRES_DATABASE}"
-
-app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['SQLALCHEMY_DATABASE_URI'] = prod_conn_str
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db.init_app(app)
-migrate = Migrate(app, db)
 
 
 @app.route("/")
