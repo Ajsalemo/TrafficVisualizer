93a8a565225121bcf8359d48b2af2de06bde20fc
Anthony Salemo <ajssalemo@gmail.com>
2020-11-16 11:21:58 -0500

    abstracting saved locations to display on a profile into it's own component

---
+++ client/src/components/SavedLocations/SavedLocations.js
@@ -0,0 +1,11 @@
+// TODO - component to retrieve saved locations based on the logged in user
+const SavedLocations = () => {
+  return (
+    <span className="text-white block pt-12">
+      You currently have no saved traffic locations{" "}
+      <i className="fas fa-sad-cry text-blue-700"></i>
+    </span>
+  );
+};
+
+export default SavedLocations;
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -2,6 +2,7 @@ import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import Footer from "../../components/Footer/Footer";
 import LoadingPageComponent from "../../components/LoadingPageComponent/LoadingPageComponent";
 import ProfileImage from "../../components/ProfileImage/ProfileImage";
+import SavedLocations from "../../components/SavedLocations/SavedLocations"
 
 const Profile = ({ error }) => {
   const { user } = useAuth0();
@@ -13,10 +14,7 @@ const Profile = ({ error }) => {
         <span className="text-white">
           Hi, {user.nickname ? user.nickname : "Guest"}
         </span>
-        <span className="text-white block pt-12">
-          You currently have no saved traffic locations{" "}
-          <i className="fas fa-sad-cry text-blue-700"></i>
-        </span>
+        <SavedLocations />
         {error && (
           <span className="text-red-700 block pt-12">
             An error has occured <i className="fas fa-sad-cry text-red-700"></i>
