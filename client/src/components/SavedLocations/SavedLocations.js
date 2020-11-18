bedd7c13ca9183279ba8843007d041c75889cae3
Anthony Salemo <ajssalemo@gmail.com>
2020-11-17 20:09:20 -0500

    adding a loading indicator for the axios call, added useEffect and a try/catch block

--- client/src/components/SavedLocations/SavedLocations.js
+++ client/src/components/SavedLocations/SavedLocations.js
@@ -1,21 +1,39 @@
-import { useEffect, useState } from "react"
+import { Fragment, useEffect, useState } from "react";
 import axios from "axios";
 
 // TODO - component to retrieve saved locations based on the logged in user
 const SavedLocations = ({ userObject }) => {
-  console.log(userObject)
-  const [savedLocations, setSavedLocations] = useState(null)
+  console.log(userObject);
+  const [savedLocations, setSavedLocations] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    const { id } = userObject;
     const getUsersSavedLocations = async () => {
-      const getRequestForSavedLocations = await axios.get(`${process.env.REACT_APP_SERVER_API_URL}/api/get_all_locations/${userObject.id}`);
-      console.log(getRequestForSavedLocations.data)
-    }
-    getUsersSavedLocations()
-  }, [])
+      setLoading(true);
+      try {
+        const getRequestForSavedLocations = await axios.get(
+          `${process.env.REACT_APP_SERVER_API_URL}/api/get_all_locations/${id}`
+        );
+        console.log(getRequestForSavedLocations.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    };
+    getUsersSavedLocations();
+  }, [userObject]);
   return (
     <span className="text-white block pt-12">
-      You currently have no saved traffic locations{" "}
-      <i className="fas fa-sad-cry text-blue-700"></i>
+      {loading ? (
+        <i className="fas fa-spinner animate-spin text-white" aria-label="loading"></i>
+      ) : (
+        <Fragment>
+          You currently have no saved traffic locations
+          <i className="fas fa-sad-cry text-blue-700"></i>
+        </Fragment>
+      )}
     </span>
   );
 };
