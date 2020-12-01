57abd0bb75eded689edf062debb15080237b624c
Anthony Salemo <ajssalemo@gmail.com>
2020-12-01 11:09:10 -0500

    changed migrate.sh

--- server/migrate.sh
+++ server/migrate.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 # Simple migration script to save some keystrokes
-
+pipenv shell 
 # Initialize the database(if needed)
-flask db init
-
+flask db init 
 # Run migrations
-flask db migrate
-
+flask db migrate 
 # Run upgrades(if any)
-flask db upgrade
-
+flask db upgrade 
 # Start Flask after migrations
-export FLASK_ENV=development
-export FLASK_APP=app.py
-echo "Flask App is set to $FLASK_APP in an $FLASK_ENV environment"
+export FLASK_ENV=development 
+export FLASK_APP=app.py 
+echo "Flask App is set to $FLASK_APP in an $FLASK_ENV environment" && \
 flask run
